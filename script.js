b7a221d950f0ac0d710cbf5901e347cc1cede773
Aspiress <rafiussyakban@gmail.com>
2024-12-26 05:36:47 +0700

    Beranda galerry improvement

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ if (document.title == "Beranda - Warung Pak Pol") {
 }
 
 
+// Mobile SideBar
 document.addEventListener("DOMContentLoaded", () => {
     const sidebar = document.getElementById('sidebar');
     const overlay = document.getElementById('overlay');
@@ -43,32 +44,40 @@ document.addEventListener("DOMContentLoaded", () => {
 });
   
 
+
+// Galeri index.html
 let slideIndex = 1;
-  showSlides(slideIndex);
+showSlides(slideIndex);
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+// Ganti gambar otomatis interval
+setInterval(function() {
+  plusSlides(1);
+}, 3000);
 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" opacity-100", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " opacity-100";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" opacity-100", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " opacity-100";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
     
